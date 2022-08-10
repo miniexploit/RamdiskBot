7f696ee07135087f15147b05ad7a222b674a15d9
mini_exploit <61931266+Mini-Exploit@users.noreply.github.com>
2022-08-10 18:05:20 +0700

    forgot BuildID

--- patcher.py
+++ patcher.py
@@ -34,13 +34,14 @@ def getDevicesJson():
 
 
 class ramdiskMaker:
-	def __init__(self, identifier, version, callback=None):
+	def __init__(self, identifier, version, buildid=None, callback=None):
 		self.callback = callback if callback else print
-		async_run(self.callback(f"init: {identifier}, {version}, {callback}"))
+		async_run(self.callback(f"init: {identifier}, {version}, {buildid}, {callback}"))
 		self.identifier = identifier
 		if "iPhone" not in self.identifier and "iPad" not in self.identifier and "iPod" not in self.identifier:
 			raise InitError("Product is not supported")
 		self.version = version
+		self.buildid = buildid
 		self.verint = int(version[:2])
 		if not 11 <= self.verint <= 14:
 			raise InitError("Version is not supported")
@@ -56,7 +57,7 @@ class ramdiskMaker:
 
 	def isOutExists(self):
 		try:
-			return utils.read_key(self.tempdir)
+			return utils.read_key(f"{self.tempdir}_{self.buildid}")
 		except:
 			return None
 
@@ -65,22 +66,37 @@ class ramdiskMaker:
 		return z.extract(remotePath, outDir)
 
 	def getFirmwareUrl(self):
-		r = requests.get(f"https://api.ipsw.me/v2.1/{self.identifier}/{self.version}/url")
+		if not self.buildid:
+			async_run(self.callback("Requesting firmware URL with identifier"))
+			r = requests.get(f"https://api.ipsw.me/v2.1/{self.identifier}/{self.version}/url")
+		else:
+			async_run(self.callback("Requesting firmware URL with BuildID"))
+			r = requests.get(f"https://api.ipsw.me/v2.1/{self.identifier}/{self.version}/url")
 		async_run(self.callback(r.text))
 		try: # this version has multiple BuildIDs
 			return r.json()[0], True # isJson(True)
 		except:
 			return r.text, False # isJson(False
 
-	def setFirmwareUrl(self, url):
+	def setFirmwareUrl(self, url, buildid=None):
+		if buildid:
+			self.buildid = buildid
+			async_run(self.callback(f"ProductBuildVersion: {self.buildid}"))
 		self.url = url
 		async_run(self.callback(f"Firmware URL set to {self.url}"))
 
-	def extractRamdisk(self):
-		async_run(self.callback("Extracting ramdisk"))
+	def loadManifest(self):
+		async_run(self.callback("Extracting BuildManifest"))
 		buildManifestPath = self.extractFile("BuildManifest.plist", self.tempdir)
 		with open(buildManifestPath, "rb") as f:
 			_plist = plistlib.load(f)
+		if not self.buildid:
+			async_run(self.callback(f"ProductBuildVersion: {self.buildid}"))
+			self.buildid = _plist["ProductBuildVersion"]
+		return _plist
+
+	def extractRamdisk(self, _plist):
+		async_run(self.callback("Extracting ramdisk"))
 		# get BuildIdentity
 		restoreramdiskpath = None
 		for buildidentity in _plist["BuildIdentities"]:
@@ -145,23 +161,32 @@ class ramdiskMaker:
 		if not os.path.exists(f"out/{self.tempdir}"):
 			os.makedirs(f"out/{self.tempdir}")
 		async_run(self.callback("Repacking ramdisk"))
-		ret = subprocess.run(["img4","-i",f"{self.tempdir}/ramdisk.dmg","-o",f"ramdisk_{self.tempdir}.im4p","-A","-T","rdsk"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		ret = subprocess.run(["img4","-i",f"{self.tempdir}/ramdisk.dmg","-o",f"ramdisk_{self.tempdir}_{self.buildid}.im4p","-A","-T","rdsk"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		if ret.returncode != 0:
 			raise ProgressError(f"Repacking ramdisk failed with return code: {ret.returncode}\nstdout={ret.stdout}\nstderr={ret.stderr}")
 		async_run(self.callback("patch done"))
-		return f"ramdisk_{self.tempdir}.im4p"
+		return f"ramdisk_{self.tempdir}_{self.buildid}.im4p"
 
 	def uploadRamdisk(self, path):
 		async_run(self.callback("Uploading ramdisk"))
-		# Find some way to upload the ramdisk yourself
-		# and save the URL to url.json
-		# and return the URL here
-		return url
+		ret = subprocess.run(["gdrive","upload",path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		if ret.returncode != 0:
+			raise ProgressError(f"Failed to upload ramdisk\nreturn code: {ret.returncode}\nstdout={ret.stdout}\nstderr={ret.stderr}")
+		# parse output to get fileid
+		line = ret.stdout.decode("utf-8").splitlines()[1]
+		# line looks like this: Uploaded FILEID at /s, total
+		fileid = line.split(" ")[1]
+		async_run(self.callback(fileid))
+		ret = subprocess.run(["gdrive","share",fileid], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		if ret.returncode != 0:
+			raise ProgressError(f"Failed to publish (share) ramdisk\nreturn code: {ret.returncode}\nstdout={ret.stdout}\nstderr={ret.stderr}")
+		utils.write_key(f"{self.tempdir}_{self.buildid}", f"https://drive.google.com/file/d/{fileid}/view?usp=sharing")
+		return f"https://drive.google.com/file/d/{fileid}/view?usp=sharing"
 
 	def cleanUp(self):
 		async_run(self.callback("Cleaning up"))
-		if os.path.isfile(f"ramdisk_{self.tempdir}.im4p"):
-			os.remove(f"ramdisk_{self.tempdir}.im4p")
+		if os.path.isfile(f"ramdisk_{self.tempdir}_{self.buildid}.im4p"):
+			os.remove(f"ramdisk_{self.tempdir}_{self.buildid}.im4p")
 		if os.path.exists(self.tempdir):
 			shutil.rmtree(self.tempdir)
 
